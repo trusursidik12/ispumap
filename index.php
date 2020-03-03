ee553d693244b955364cec6978cc6e408dbc9af9
rizaldi <rizaldi354313@gmail.com>
2020-03-03 11:03:35 +0700

    bootrstarp and fontawesome

--- index.php
+++ index.php
@@ -115,6 +115,16 @@ $bulan = [
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
 
     <style>
@@ -470,11 +480,6 @@ $bulan = [
     </style>
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-md bg-light fixed-top">
-            <a class="navbar-brand" href="#"><img src="assets/airmap_logo.png" height="59px"></a>
-        </nav>
-    </header>
     <div class="close" id="sidebar-container">
         <div id="sidebar-toggler"><button class="btn btn-primary" type="button" ><i class="fa fa-caret-right"></i><i class="fa fa-caret-left"></i></button></div>
         <div class="content">
@@ -578,6 +583,26 @@ $bulan = [
     </div>
 
     <div id="mapid"></div>
+
+    <div class="footer bg-primary fixed-bottom text-danger">
+        <div class="d-flex">
+            <div class="p-2">
+                <button><i class="fas fa-globe"></i>Home</button>
+            </div>
+            <div class="p-2">
+                <button><i class="fas fa-heart"></i>Home</button>
+            </div>
+            <div class="p-2">
+                <button><i class="fas fa-heart"></i>Home</button>
+            </div>
+            <div class="p-2">
+                <button><i class="fas fa-heart"></i>Home</button>
+            </div>
+            <div class="p-2">
+                <button><i class="fas fa-heart"></i>Home</button>
+            </div>
+        </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
