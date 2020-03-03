4b0a13e2ddf619ef395898b395d4349033752876
rizaldi <rizaldi354313@gmail.com>
2020-03-03 11:12:27 +0700

    assets

--- index.php
+++ index.php
@@ -115,16 +115,7 @@ $bulan = [
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-
     <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
 
     <style>
@@ -585,20 +576,20 @@ $bulan = [
     <div id="mapid"></div>
 
     <div class="footer bg-primary fixed-bottom text-danger">
-        <div class="d-flex">
-            <div class="p-2">
+        <div class="d-flex text-center">
+            <div class="p-2 flex-fill bg-primary">
                 <button><i class="fas fa-globe"></i>Home</button>
             </div>
-            <div class="p-2">
+            <div class="p-2 flex-fill bg-success">
                 <button><i class="fas fa-heart"></i>Home</button>
             </div>
-            <div class="p-2">
+            <div class="p-2 flex-fill bg-danger">
                 <button><i class="fas fa-heart"></i>Home</button>
             </div>
-            <div class="p-2">
+            <div class="p-2 flex-fill bg-warning">
                 <button><i class="fas fa-heart"></i>Home</button>
             </div>
-            <div class="p-2">
+            <div class="p-2 flex-fill bg-info">
                 <button><i class="fas fa-heart"></i>Home</button>
             </div>
         </div>
