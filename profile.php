0d96001a81770fbbd304c694b31609cba805e707
rizaldi <rizaldi354313@gmail.com>
2020-03-04 14:42:14 +0700

    profile

---
+++ profile.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-primary sticky-top">
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+	    <a class="navbar-brand" href="#">
+	    	<img width="60" src="icon/logo.jpeg">
+	    </a>
+	    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+	    </ul>
+	  </div>
+	</nav>
+
+	<div class="card-header">
+		<div class="card text-center">
+			<div class="col">
+				<img width="100" src="image/profile.png">
+				<h2>My Location</h2>
+				<h5>kota apa..</h5>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-header">
+				aaaaa
+			</div>
+			<div class="d-flex text-center">
+				<div class="p-2 flex-fill">
+					test
+				</div>
+				<div class="p-2 flex-fill">
+					test
+				</div>
+				<div class="p-2 flex-fill">
+					test
+				</div>
+				<div class="p-2 flex-fill">
+					test
+				</div>
+				<div class="p-2 flex-fill">
+					test
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer fixed-bottom">
+        <div class="d-flex text-center">
+            <div class="p-2 flex-fill bg-primary">
+                <a href="# " class="text-white"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="p-2 flex-fill bg-success">
+                <a href="#" class="text-white"><i class="fas fa-heart"></i></a>
+            </div>
+            <div class="p-2 flex-fill bg-danger">
+                <a href="#" class="text-white"><i class="fas fa-user-circle"></i></a>
+            </div>
+            <div class="p-2 flex-fill bg-warning">
+                <a href="#" class="text-white"><i class="fas fa-newspaper"></i></a>
+            </div>
+            <div class="p-2 flex-fill bg-info">
+                <a href="#" class="text-white"><i class="fas fa-sitemap"></i></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
