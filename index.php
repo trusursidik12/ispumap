d089a2d056da52e75c5895cc8c357d66840ff3b1
warih suryono <warih_suryono@yahoo.com>
2020-03-09 15:08:42 +0700

    aaa

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+if($_SERVER['HTTPS']!="on") {
+$redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+header("Location:$redirect");
+}
 $rank = array(
     array(
         'label_id' => 'baik',
