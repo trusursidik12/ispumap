fdc00cbf92491b134dc97717a0bacbec07556251
rizaldi <rizaldi354313@gmail.com>
2020-03-10 15:01:13 +0700

    sitemap'

---
+++ sitemap.php
@@ -0,0 +1,566 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>News</title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+</head>
+<body>
+
+	<div class="p-3" width="100%">
+		<div class="card text-center">
+			<div class="row text-center">
+				<div class="col-sm">
+					<h1>SITEMAP</h1>
+				</div>
+			</div>
+			<div class="row m-1">
+				<div class="col-sm">
+					<button class="btn btn-success" data-toggle="modal" data-target="#news1">22</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- The Modal -->
+	<div class="modal fade" id="news1">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<!-- Modal Header -->
+				<div class="modal-header">
+					<div class="col-lg nav nav-tabs justify-content-center sticky-top">
+						<a href="#realtime" class="nav-link btn btn-light text-center active" style="padding-left: 80px;padding-right: 80px;">Realtime</a>
+						<a href="#ispu" class="nav-link btn btn-light" style="padding-left: 80px;padding-right: 80px;">ISPU</a>
+					</div>
+				</div>
+				<!-- Modal body -->
+				<div class="modal-body">
+
+					<div class="tab-content">
+						<div id="realtime" class="container tab-pane active">
+							<img src="assets/ic_emote_bagus.png" width="40">
+							<h5 class="text-center" style="margin-top: -30px;">Status : BAIK</h5><hr style="margin-top: 0px; margin-bottom: 0px;">
+							<div class="d-flex">
+								<div class="col-sm-6">
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											PM10
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											PM25
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											SO2
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											CO
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											O3
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											NO2
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											HC
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											VOC
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											NH3
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											NO
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											H2S
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											CS2
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-success">
+												22
+											</div>
+										</div>
+									</div>							
+								</div>
+							</div>
+							<hr style="margin-top: 5px; margin-bottom: 5px;">
+							<div class="d-flex">
+								<div class="col-sm-3">
+									<div class="row m-1">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/cloud.png">
+											<p><b>30°</b></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="row m-1">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/humidity2.png">
+											<p><b>56%</b></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="row m-1">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/wind.png">
+											<p><b style="font-size: 12px">250</b><b style="font-size: 9px;">km/h</b></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="row m-1 text-center">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/mask2.png">
+											<p><b>37</b></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- end tab isi -->
+						<div id="ispu" class="container tab-pane fade">
+							<img src="assets/ic_emote_sedang.png" width="40">
+							<h5 class="text-center" style="margin-top: -30px;">Status : SEDANG</h5><hr style="margin-top: 0px; margin-bottom: 0px;">
+							<div class="d-flex">
+								<div class="col-sm-6">
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											PM10
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											PM25
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											SO2
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											CO
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											O3
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											NO2
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											HC
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											VOC
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											NH3
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											NO
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											H2S
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>
+									<div class="row m-1">
+										<div class="col-sm p-1">
+											CS2
+										</div>
+										<div class="col-sm">
+											<div class="card p-1 text-center bg-primary">
+												51
+											</div>
+										</div>
+									</div>							
+								</div>
+							</div>
+							<hr style="margin-top: 5px; margin-bottom: 5px;">
+							<div class="d-flex">
+								<div class="col-sm-3">
+									<div class="row m-1">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/cloud.png">
+											<p><b>31°</b></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="row m-1">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/humidity2.png">
+											<p><b>58%</b></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="row m-1">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/wind.png">
+											<p><b style="font-size: 12px">145</b><b style="font-size: 9px;">km/h</b></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="row m-1 text-center">
+										<div class="col-sm p-3 text-center">
+											<img src="assets/mask2.png">
+											<p><b>76</b></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- end tab isi -->
+					</div>
+					<!-- end tab-modal -->
+					<hr style="margin-top: 0px;margin-bottom: 5px;">
+					<div class="bg-light">
+						<h5 class="text-center">Prediksi Esok Hari</h5>
+					</div>
+					<hr style="margin-top: 0px;margin-bottom: 0px;">
+					<div class="d-flex">
+						<div class="col-sm-6">
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									PM10
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									PM25
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									SO2
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									CO
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									O3
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									NO2
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									HC
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									VOC
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									NH3
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									NO
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									H2S
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>
+							<div class="row m-1">
+								<div class="col-sm p-1">
+									CS2
+								</div>
+								<div class="col-sm">
+									<div class="card p-1 text-center bg-warning">
+										115
+									</div>
+								</div>
+							</div>							
+						</div>
+					</div>
+					<div class="d-flex">
+						<div class="col-sm-3">
+							<div class="row m-1">
+								<div class="col-sm p-3 text-center">
+									<img src="assets/cloud.png">
+									<p><b>26°</b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="row m-1">
+								<div class="col-sm p-3 text-center">
+									<img src="assets/humidity2.png">
+									<p><b>76%</b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="row m-1">
+								<div class="col-sm p-3 text-center">
+									<img src="assets/wind.png">
+									<p><b style="font-size: 12px">45</b><b style="font-size: 9px;">km/h</b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="row m-1 text-center">
+								<div class="col-sm p-3 text-center">
+									<img src="assets/mask2.png">
+									<p><b>126</b></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- end modal -->
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+	<script>
+	$(document).ready(function(){
+	  $(".nav-tabs a").click(function(){
+	    $(this).tab('show');
+	  });
+	});
+	</script>
+
+</body>
+</html>
